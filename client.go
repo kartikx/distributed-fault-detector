2a0706aa01b72d19abd275be8cca8277f8984751
Sriram <sriramdvt@gmail.com>
2024-09-28 18:31:34 -0500

    Added logging and corrected ping intervals

--- client.go
+++ client.go
@@ -20,103 +20,107 @@ func startClient(clientServerChan chan int) {
 		Shuffle(members)
 
 		for _, nodeId := range members {
-			connection := GetNodeConnection(nodeId)
-
-			if connection == nil {
-				fmt.Printf("Node %s connection is nil, it might have been removed from the group\n", nodeId)
-				continue
-			}
-
-			var messagesToPiggyback Messages = GetUnexpiredPiggybackMessages()
-
-			pingMessageEnc, err := EncodePingMessage(messagesToPiggyback)
-			if err != nil {
-				fmt.Println("Unable to encode ping message")
-				continue
-			}
-
-			var pingMessage Message
-			err = json.Unmarshal(pingMessageEnc, &pingMessage)
-			if err != nil {
-				fmt.Println("Unable to decode outgoing PING message")
-				continue
-			}
-			printMessage("outgoing", pingMessage, nodeId)
-
-			connection.Write(pingMessageEnc)
-
-			buffer := make([]byte, 1024)
-
-			// TODO would this work would even if I were to re-use the connection?
-			connection.SetReadDeadline(time.Now().Add(TIMEOUT_DETECTION_MILLISECONDS * time.Millisecond))
-			mLen, err := connection.Read(buffer)
-
-			if err != nil {
-				fmt.Printf("%s timed out\n", nodeId)
-
-				// In suspicion, you would want to suspect it first.
-				if inSuspectMode {
-					// Create a SUSPECT message to process and disseminate
-					member, _ := GetMemberInfo(nodeId)
-					suspectMessage := Message{Kind: SUSPECT, Data: strconv.Itoa(member.incarnation) + "@" + nodeId}
-					printMessage("outgoing", suspectMessage, suspectMessage.Data)
-					go ProcessSuspectMessage(suspectMessage)
-
-					continue
-				} else { // Otherwise, just mark the node as failed
-
-					DeleteMember(nodeId)
-
-					// Start propagating FAIL message.
-					failedMessage := Message{
-						Kind: FAIL,
-						Data: nodeId,
-					}
-
-					AddPiggybackMessage(failedMessage, len(membershipInfo))
-
-					continue
-				}
-			}
-			// TODO simulate drops on receiver end.
-
-			messages, err := DecodeAckMessage(buffer[:mLen])
-			// fmt.Println("Messages in ACK: ", len(messages))
-			if err != nil {
-				fmt.Printf("Unable to decode ACK message from node: %s", nodeId)
-				continue
-			}
-
-			var ackMessage Message
-			err = json.Unmarshal(buffer[:mLen], &ackMessage)
-			if err != nil {
-				continue
-			}
-			printMessage("incoming", ackMessage, nodeId)
-
-			for _, subMessage := range messages {
-				switch subMessage.Kind {
-				case HELLO:
-					ProcessHelloMessage(subMessage)
-				case LEAVE:
-					ProcessFailOrLeaveMessage(subMessage)
-				case FAIL:
-					ProcessFailOrLeaveMessage(subMessage)
-				case SUSPECT:
-					go ProcessSuspectMessage(subMessage)
-				case ALIVE:
-					ProcessAliveMessage(subMessage)
-				case SUSPECT_MODE:
-					ProcessSuspectModeMessage(subMessage)
-				default:
-					log.Fatalf("Unexpected submessage kind in ACK")
-				}
-			}
-
-			// TODO remove.
+			go handleEachMember(nodeId)
 			time.Sleep(PING_INTERVAL_MILLISECONDS * time.Millisecond)
 		}
 	}
+}
+
+func handleEachMember(nodeId string) {
+
+	connection := GetNodeConnection(nodeId)
+
+	if connection == nil {
+		fmt.Printf("Node %s connection is nil, it might have been removed from the group\n", nodeId)
+		return
+	}
+
+	var messagesToPiggyback Messages = GetUnexpiredPiggybackMessages()
+
+	pingMessageEnc, err := EncodePingMessage(messagesToPiggyback)
+	if err != nil {
+		fmt.Println("Unable to encode ping message")
+		return
+	}
+
+	var pingMessage Message
+	err = json.Unmarshal(pingMessageEnc, &pingMessage)
+	if err != nil {
+		fmt.Println("Unable to decode outgoing PING message")
+		return
+	}
+	printMessage("outgoing", pingMessage, nodeId)
+
+	connection.Write(pingMessageEnc)
+
+	buffer := make([]byte, 1024)
+
+	// TODO would this work would even if I were to re-use the connection?
+	connection.SetReadDeadline(time.Now().Add(TIMEOUT_DETECTION_MILLISECONDS * time.Millisecond))
+	mLen, err := connection.Read(buffer)
+
+	if err != nil {
+		fmt.Printf("%s timed out\n", nodeId)
+
+		// In suspicion, you would want to suspect it first.
+		if inSuspectMode {
+			// Create a SUSPECT message to process and disseminate
+			member, _ := GetMemberInfo(nodeId)
+			suspectMessage := Message{Kind: SUSPECT, Data: strconv.Itoa(member.incarnation) + "@" + nodeId}
+			printMessage("outgoing", suspectMessage, suspectMessage.Data)
+			go ProcessSuspectMessage(suspectMessage)
+
+			return
+		} else { // Otherwise, just mark the node as failed
+
+			DeleteMember(nodeId)
+
+			// Start propagating FAIL message.
+			failedMessage := Message{
+				Kind: FAIL,
+				Data: nodeId,
+			}
+
+			AddPiggybackMessage(failedMessage, len(membershipInfo))
+
+			return
+		}
+	}
+	// TODO simulate drops on receiver end.
+
+	messages, err := DecodeAckMessage(buffer[:mLen])
+	// fmt.Println("Messages in ACK: ", len(messages))
+	if err != nil {
+		fmt.Printf("Unable to decode ACK message from node: %s", nodeId)
+		return
+	}
+
+	var ackMessage Message
+	err = json.Unmarshal(buffer[:mLen], &ackMessage)
+	if err != nil {
+		return
+	}
+	printMessage("incoming", ackMessage, nodeId)
+
+	for _, subMessage := range messages {
+		switch subMessage.Kind {
+		case HELLO:
+			ProcessHelloMessage(subMessage)
+		case LEAVE:
+			ProcessFailOrLeaveMessage(subMessage)
+		case FAIL:
+			ProcessFailOrLeaveMessage(subMessage)
+		case SUSPECT:
+			go ProcessSuspectMessage(subMessage)
+		case ALIVE:
+			ProcessAliveMessage(subMessage)
+		case SUSPECT_MODE:
+			ProcessSuspectModeMessage(subMessage)
+		default:
+			log.Fatalf("Unexpected submessage kind in ACK")
+		}
+	}
+
 }
 
 func ExitGroup() {
--- logger.go
+++ logger.go
@@ -1,3 +1,75 @@
 // Implements logging functionality.
 
 package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+)
+
+var log_file, _ = os.OpenFile("./machine.log", os.O_WRONLY|os.O_CREATE, 0666)
+var log_file_writer = bufio.NewWriter(log_file)
+
+func printMessage(direction string, message Message, nodeId string) {
+	fmt.Fprintf(log_file_writer, "---------\nPrinting %s message\n%s\n", direction, time.Now())
+	switch message.Kind {
+	case PING:
+		fmt.Fprintf(log_file_writer, "PING message (%s):\n", nodeId)
+
+		var messages Messages
+		err := json.Unmarshal([]byte(message.Data), &messages)
+		if err != nil {
+			fmt.Fprintf(log_file_writer, "Failed to unmarshal PING submessages")
+			return
+		}
+
+		fmt.Fprintf(log_file_writer, "Submessages vvvvv\n")
+		for _, subMessage := range messages {
+			printMessage(direction, subMessage, nodeId)
+		}
+		fmt.Fprintf(log_file_writer, "Submessages ^^^^^\n")
+	case ACK:
+		fmt.Fprintf(log_file_writer, "ACK message (%s):\n", nodeId)
+
+		var messages Messages
+		err := json.Unmarshal([]byte(message.Data), &messages)
+		if err != nil {
+			fmt.Fprintf(log_file_writer, "Failed to unmarshal PING submessages")
+			return
+		}
+
+		fmt.Fprintf(log_file_writer, "Submessages vvvvv\n")
+		for _, subMessage := range messages {
+			printMessage(direction, subMessage, nodeId)
+		}
+		fmt.Fprintf(log_file_writer, "Submessages ^^^^^\n")
+
+	case JOIN:
+		fmt.Fprintf(log_file_writer, "JOIN message with %s\n", message.Data)
+
+	case LEAVE:
+		fmt.Fprintf(log_file_writer, "LEAVE message with %s\n", message.Data)
+
+	case FAIL:
+		fmt.Fprintf(log_file_writer, "FAIL message with %s\n", message.Data)
+
+	case HELLO:
+		fmt.Fprintf(log_file_writer, "HELLO message with %s\n", message.Data)
+
+	case SUSPECT:
+		fmt.Fprintf(log_file_writer, "SUSPECT message with %s\n", message.Data)
+
+	case ALIVE:
+		fmt.Fprintf(log_file_writer, "ALIVE message with %s\n", message.Data)
+
+	case SUSPECT_MODE:
+		fmt.Fprintf(log_file_writer, "SUSPECT_MODE message with %s\n", message.Data)
+
+	default:
+		fmt.Fprintf(log_file_writer, "********Trying to print unknown message type**********")
+	}
+	fmt.Fprintf(log_file_writer, "---------\n")
+}
