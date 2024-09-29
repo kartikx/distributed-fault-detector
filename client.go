1379ce49234ca04b5d818986568d529adffd4127
Kartik Ramesh <kartikr2@illinois.edu>
2024-09-29 17:48:19 -0500

    Add error message to conn

--- client.go
+++ client.go
@@ -84,7 +84,7 @@ func handleEachMember(nodeId string) {
 
 			return
 		} else { // Otherwise, just mark the node as failed
-			fmt.Printf("Error in reading from connection for nodeId [%s]\n", nodeId)
+			fmt.Printf("Error in reading from connection for nodeId [%s] Err: [%s]\n", nodeId, err.Error())
 
 			LogMessage(fmt.Sprintf("DETECTED NODE %s as FAILED", nodeId))
 			DeleteMember(nodeId)
